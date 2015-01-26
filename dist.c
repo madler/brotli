999a3d94e5d73b407fe1a59bea315bd5faae9114
Mark Adler <madler@alumni.caltech.edu>
2015-01-25 20:20:53 -0800

    Generate all possible distance codes with extra bits.

---
+++ dist.c
@@ -0,0 +1,34 @@
+/* Show all brotli distance codes that have extra bits. */
+
+#include <stdio.h>
+
+void codes(int p)
+{
+    int d, x, b;
+
+    printf("NPOSTFIX = %d\n", p);
+    d = 0;
+    for (;;) {
+        x = 1 + (d >> (p + 1));
+        if (x > 24)
+            break;
+        b = ((((2 + ((d >> p) & 1)) << x) - 4) << p) + (d & ((1 << p) - 1));
+        if (x == 1)
+            printf("%3d: %d, %d\n",
+                   d, b, b + (1 << p));
+        else
+            printf("%3d: %d, %d, ..., %d\n",
+                   d, b, b + (1 << p), b + (((1 << x) - 1) << p));
+        d++;
+    }
+    printf("%d codes (expect %d)\n\n", d, 48 << p);
+}
+
+int main(void)
+{
+    codes(0);
+    codes(1);
+    codes(2);
+    codes(3);
+    return 0;
+}
