5d4b5480d0539ead71e1ca66cc4210cf29a34c82
Mark Adler <madler@alumni.caltech.edu>
2015-04-12 18:39:49 -0700

    Fix deb from stdin, including forcing binary mode.

--- deb.c
+++ deb.c
@@ -1,9 +1,20 @@
-/* Decompress a brotli stream from stdin using yeast. */
+/* Decompress a brotli streams on the command line or from stdin using yeast.
+   The compressed output is written to the same name with the suffix ".bro" or
+   ".compressed" removed and ".out" added, or to "deb.out" when reading from
+   stdin. */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "yeast.h"
+
+#if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(__CYGWIN__)
+#  include <fcntl.h>
+#  include <io.h>
+#  define SET_BINARY_MODE(file) setmode(fileno(file), O_BINARY)
+#else
+#  define SET_BINARY_MODE(file)
+#endif
 
 /* Load the entire file into a memory buffer.  load() returns 0 on success, in
    which case it puts all of the file data in *dat[0..*len - 1].  That is,
@@ -177,6 +188,7 @@ int main(int argc, char **argv)
 
     /* or if no names on the remaining command line, decompress from stdin */
     else {
+        SET_BINARY_MODE(stdin);
         ret = load(stdin, &source, &len, 0);
         if (ret) {
             fputs(ret > 0 ? "out of memory\n" : "error reading stdin\n",
@@ -187,7 +199,7 @@ int main(int argc, char **argv)
         fprintf(stderr, "uncompressed length = %zu\n", got);
         if (ret)
             fprintf(stderr, "yeast() returned %d\n", ret);
-        deliver(NULL, dest, got);
+        deliver("deb", dest, got);
         free(dest);
         free(source);
     }
