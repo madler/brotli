c2941111100e19ab0a3d836417cc9dbc52b80b12
Mark Adler <madler@alumni.caltech.edu>
2015-04-19 19:50:15 -0700

    Compute total length of prefixes and suffixes of transforms.

--- xflen.c
+++ xflen.c
@@ -24,17 +24,21 @@ typedef struct {
 
 int main(void)
 {
-    size_t n, len, maxp = 0, maxs = 0;
+    size_t n, len, ps = 0, ss = 0, maxp = 0, maxs = 0;
 
     for (n = 0; n < sizeof(xforms) / sizeof(xform_t); n++) {
         len = strlen(xforms[n].prefix);
+        ps += len;
         if (len > maxp)
             maxp = len;
         len = strlen(xforms[n].suffix);
+        ss += len;
         if (len > maxs)
             maxs = len;
     }
     printf("%zu transforms: max prefix = %zu, max suffix = %zu\n",
            n, maxp, maxs);
+    printf("total prefix bytes = %zu, total suffix bytes = %zu\n",
+           ps, ss);
     return 0;
 }
