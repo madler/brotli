26b90fa2d1c7a9da5471aec80cfc81c9f176115c
Mark Adler <madler@alumni.caltech.edu>
2023-12-07 10:56:52 -0800

    Permit size to be NULL in load().

--- load.c
+++ load.c
@@ -1,16 +1,15 @@
 #include <stdlib.h>
+#include <stddef.h>
 #include "load.h"
 
-// Return true if there is more to read from in.  This function is used only if
+// Return true if there is more to read from in. This function is used only if
 // a memory allocation or reallocation fails, or if the data fills limit.
-static inline int more(FILE *in)
-{
+static inline int more(FILE *in) {
     return ungetc(getc(in), in) != EOF;
 }
 
 // See load.h for description.
-int load(FILE *in, size_t limit, void **dat, size_t *size, size_t *len)
-{
+int load(FILE *in, size_t limit, void **dat, size_t *size, size_t *len) {
     // if no input limit, then set it to the maximum size_t value
     if (limit == 0)
         limit--;
@@ -19,11 +18,13 @@ int load(FILE *in, size_t limit, void **dat, size_t *size, size_t *len)
     *len = 0;
 
     // assure that *dat has an allocation to read into
-    if (*dat == NULL) {
-        *size = (size_t)1 << 15;    // 32 KiB
-        *dat = malloc(*size);
+    size_t have = size == NULL ? 0 : *size;
+    if (*dat == NULL || have == 0) {
+        have = (size_t)1 << 15;    // 32 KiB
+        *dat = realloc(*dat, have);
         if (*dat == NULL) {
-            *size = 0;
+            if (size != NULL)
+                *size = 0;
             return more(in) ? -2 : 0;
         }
     }
@@ -31,35 +32,39 @@ int load(FILE *in, size_t limit, void **dat, size_t *size, size_t *len)
     // read from in until no more input, the limit is reached, or out of memory
     for (;;) {
         // read as much as is available into the allocated space up to limit
-        {
-            size_t max = *size > limit ? limit : *size;
-            *len += fread(*dat + *len, 1, max - *len, in);
-            if (ferror(in))
-                return -1;
-            if (*len < max || *size >= limit)
-                break;
+        size_t max = have > limit ? limit : have;
+        *len += fread(*dat + *len, 1, max - *len, in);
+        if (ferror(in)) {
+            if (size != NULL)
+                *size = have;
+            return -1;
         }
+        if (*len < max || have >= limit)
+            break;
 
         // increase size to the next power of two, saturating to the maximum
-        // size_t value
-        {
-            size_t reduce, bigger;
-            reduce = *size;
-            do {
-                bigger = reduce;
-                reduce &= reduce - 1;
-            } while (reduce);
-            bigger <<= 1;
-            if (bigger == 0)
-                bigger--;
-            void *mem = realloc(*dat, bigger);
-            if (mem == NULL)
-                return more(in) ? -2 : 0;
-            *dat = mem;
-            *size = bigger;
+        // ptrdiff_t value
+        size_t reduce = have;
+        size_t bigger;
+        do {
+            bigger = reduce;
+            reduce &= reduce - 1;
+        } while (reduce);
+        bigger <<= 1;
+        if ((ptrdiff_t)bigger < 0)
+            bigger--;
+        void *mem = realloc(*dat, bigger);
+        if (mem == NULL) {
+            if (size != NULL)
+                *size = have;
+            return more(in) ? -2 : 0;
         }
+        *dat = mem;
+        have = bigger;
     }
 
     // return 0 on success, 1 if limit was reached with unread input
+    if (size != NULL)
+        *size = have;
     return *len == limit && more(in) ? 1 : 0;
 }
--- load.h
+++ load.h
@@ -1,25 +1,26 @@
-/* Load the entire input from in into a memory buffer.  The allocated memory
+/* Load the entire input from in into a memory buffer. The allocated memory
    buffer is returned in *dat, which points to the loaded data, with the number
    of bytes of data at *dat returned in *len.
 
    If limit is not zero, then *len is constrained to be less than or equal to
-   limit.  If limit is zero, then *len is only limited to the maximum size_t
-   value.  The file pointer is left at the end of the file, or pointing after
+   limit. If limit is zero, then *len is only limited to the maximum size_t
+   value. The file pointer is left at the end of the file, or pointing after
    the *len or maximum size_t bytes that were read.
 
    If *dat on entry is not NULL, then it is assumed to be an allocated buffer
-   of size *size.  If *dat on entry is NULL, then a new buffer is allocated and
-   the entry value of *size is ignored and overwritten.  A supplied buffer is
+   of size *size. If *dat on entry is NULL, then a new buffer is allocated and
+   the entry value of *size is ignored and overwritten. A supplied buffer is
    never reduced in size, but may be increased in size which may result in a
-   new address, returned in *dat.  In any case, *size returns the final size of
-   the allocation at *dat.  This enables reuse of an existing allocation over
-   multiple load() calls.
+   new address, returned in *dat. In any case, *size returns the final size of
+   the allocation at *dat. This enables reuse of an existing allocation over
+   multiple load() calls. size can be NULL, in which case the size of the
+   allocation is not returned, and reuse is not supported.
 
    load() returns 0 on success, 1 if limit or the maximum size_t value was
    reached with more data remaining in the input, -1 if there is a read error,
-   or -2 if out of memory.  In all cases, *dat, *size, and *len return with
+   or -2 if out of memory. In all cases, *dat, *size, and *len return with
    their last values, so *dat contains *len bytes from in, and *dat is an
-   allocation of size *size.
+   allocation of size *size, unless size is NULL.
  */
 
 #include <stdio.h>
